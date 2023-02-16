166197b8e2917e0a8c0b7d12e19dae8124a70f70
KimuchuJr <kimuchukim@gmail.com>
2023-02-16 07:29:16 -0800

    Advanced tasks

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ *
+ * Return 1 (success)
+ */
+
+int main(void)
+{
+	fprintf(stderr, "%s", "and that piece of art is useful\"");
+	fprintf(stderr, "%s", " - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,13 +6,12 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-
 {
-printf("size of char is=%i byte(s)\n", sizeof(char));
-printf("size of int is=%i byte(s)\n", sizeof(int));
-printf("size of long int is=%i byte(s)\n", sizeof(long int));
-printf("size of long long int is=%i byte(s)\n", sizeof(long long int));
-printf("size of float is=%i byte(s)\n", sizeof(float));
-return (0);
+	printf("Size of a char: %ld byte(s)\n", sizeof(char));
+	printf("Size of an int: %ld byte(s)\n", sizeof(int));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %ld byte(s)\n", sizeof(float));
+	return (0);
 }
 
