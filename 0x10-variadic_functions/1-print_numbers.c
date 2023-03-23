1bea6a182dc7bba282fdb5561d304ebe9b96a45f
KimuchuJr <kimuchukim@gmail.com>
2023-03-23 12:41:00 -0700

    A function that prints numbers followed by a new line

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - print a variable number of integers using a separator
+ *
+ * @separator: String operator to print
+ * @n: number of integers
+ *
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list ap;
+
+	va_start(ap, n);
+	for (i = 0; i < n; ++i)
+	{
+		printf("%d", va_arg(ap, int));
+		if (i != n - 1 && separator)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(ap);
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,23 @@
 #ifndef _VARIADIC_FUNCTIONS_H_
 #define _VARIADIC_FUNCTIONS_H_
 
+/**
+ * struct print_types - structure to print different data types
+ *
+ * @arg: character to specify the data type
+ * @print: pointer to a function to print a specific type
+ *
+ */
+typedef struct print_types
+{
+	char arg;
+	void (*print_func)(va_list);
+} p_type;
+
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char *const format, ...);
 
 #endif
