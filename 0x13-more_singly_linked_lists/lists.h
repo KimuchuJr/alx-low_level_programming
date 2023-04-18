7cdbe0538223f74db1c35378047c961e24541280
KimuchuJr <kimuchukim@gmail.com>
2023-04-18 09:37:55 -0700

    Update header file

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -16,14 +16,14 @@
  */
 typedef struct listint_s
 {
-	int n;
+	int b;
 	struct listint_s *next;
 } listint_t;
 
 int _putchar(char c);
 size_t print_listint(const listint_t *c);
 size_t listint_len(const listint_t *h);
-listint_t *add_nodeint(listint_t **head, const int n);
+listint_t *add_nodeint(listint_t **header, const int b);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 void free_listint2(listint_t **head);
