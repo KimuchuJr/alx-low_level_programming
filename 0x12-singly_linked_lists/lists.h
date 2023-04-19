59ec51114def341a4079115e9035f3bd7311c4dd
KimuchuJr <kimuchukim@gmail.com>
2023-04-19 05:12:46 -0700

    Update header file

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -22,7 +22,7 @@ size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **header, const char *str);
 list_t *add_node_end(list_t **header, const char *str);
-void free_list(list_t *head);
+void free_list(list_t *header);
 void print_first(void);
 
 #endif
