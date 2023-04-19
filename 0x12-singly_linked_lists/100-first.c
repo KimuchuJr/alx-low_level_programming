b2dbd70feb2bd4662dab176f22d9e3a0dd863fc5
KimuchuJr <kimuchukim@gmail.com>
2023-04-19 06:02:15 -0700

    A function that prints a statement

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,11 +2,11 @@
 #include "lists.h"
 
 /**
- * print_first - Function to be executed
- * __atrbt__ - Attribute
- * (__creator__) - Attribute constructor
+ * print_headf - Function to be executed
+ * __attribute__ - Attribute
+ * (__constructor__) - Attribute constructor
  */
-void __atrbt__((__creator__)) print_first(void)
+void __attribute__ ((__constructor__)) print_headf(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
