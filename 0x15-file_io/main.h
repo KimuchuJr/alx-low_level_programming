32a2d46232f5c773ed7f96179bc7e9663ad36ff7
KimuchuJr <kimuchukim@gmail.com>
2023-04-18 22:57:51 -0700

    Update header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,6 +3,6 @@
 
 int _putchar(char c);
 ssize_t read_textfile(const char *fn, size_t ltrs);
-int create_file(const char *filename, char *text_content);
-int append_text_to_file(const char *filename, char *text_content);
+int create_file(const char *fn, char *text_content);
+int append_text_to_file(const char *fn, char *text_content);
 #endif
