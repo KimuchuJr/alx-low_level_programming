7021539f6a6ceacf1e4b93de7c4f2386c79f762f
KimuchuJr <kimuchukim@gmail.com>
2023-04-19 00:06:50 -0700

    Update header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,4 +5,14 @@ int _putchar(char c);
 ssize_t read_textfile(const char *fn, size_t ltrs);
 int create_file(const char *fn, char *text_content);
 int append_text_to_file(const char *fn, char *text_content);
+
+void check_file(Elf64_Ehdr *headerf);
+void process_info(Elf64_Ehdr *headerp );
+void class(Elf64_Ehdr *headerp);
+void data(Elf64_Ehdr *headerp);
+void version(Elf64_Ehdr *headerp);
+void os_abi(Elf64_Ehdr *headerp);
+void abi_version(Elf64_Ehdr *headerp);
+void type(Elf64_Ehdr *headerp);
+void entry_point(Elf64_Ehdr *headerp);
 #endif
