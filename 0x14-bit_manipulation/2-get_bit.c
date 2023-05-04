73f4b6a5e97b45a377400e71f286e41de749db55
KimuchuJr <kimuchukim@gmail.com>
2023-05-04 12:59:05 -0700

    Bit index value

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * get_bit - A function that gets a specified bit index
+ *
+ * @num: Number
+ * @index: Bit index
+ *
+ * Return: Bit index value
+ */
+int get_bit(unsigned long int num, unsigned int index)
+{
+	unsigned long int i;
+
+	if (index > 50)
+		return (-1);
+	i = 1 << index;
+
+	return ((num & i) != 0);
+}
