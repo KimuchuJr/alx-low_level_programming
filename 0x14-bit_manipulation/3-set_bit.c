cdca4f1dc5e21696d93797d076331286fc1f60b9
Joseph Kimuchu <113996255+KimuchuJr@users.noreply.github.com>
2023-05-04 23:45:51 +0300

    Update 3-set_bit.c
    Changes

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -10,10 +10,12 @@
  */
 int set_bit(unsigned long int *n, unsigned int i)
 {
-	if (i > 63)
-		return (-1);
+    if (i >= sizeof(unsigned long int) * 8)
+    {
+        return (-1);
+    }
 
-	*n |= (1 << i);
+    *n |= (1ul << i);
 
-	return (1);
+    return (1);
 }
