b4376074c08edd137323fdda91096bbdea27f611
Joseph Kimuchu <113996255+KimuchuJr@users.noreply.github.com>
2023-05-05 00:56:53 +0300

    Update 1-print_binary.c
    Change

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,35 +1,17 @@
 #include "main.h"
 
+#include "main.h"
+
 /**
- * print_binary - Number binary representation
+ * print_binary - prints the binary representation of a number
  *
- * @n: Index to be converted
- *
+ * @n: the number to convert
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int d;
-	int f, s;
+	if (n > 1)
+		print_binary(n >> 1);
 
-	if (n == 0)
-	{
-		_putchar('0');
-		return;
-	}
-
-	f = 0;
-	s = (sizeof(n) * 4) - 2;
-	for (d = (1 << s); d > 0; d >>= 1)
-	{
-		if ((n & d))
-		{
-			if (!f)
-				f = 1;
-			_putchar('1');
-		}
-		else if (f)
-		{
-			_putchar('0');
-		}
-	}
+	_putchar((n & 1) + '0');
 }
+
