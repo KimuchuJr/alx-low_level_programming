2687445a424df8bf8d10a20b57a04450ee1ca72c
KimuchuJr <kimuchukim@gmail.com>
2023-05-04 14:18:28 -0700

    Sets value of a bit to 0

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -6,5 +6,6 @@ void print_binary(unsigned long int n);
 int _putchar(char c);
 int get_bit(unsigned long int num, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int i);
+int clear_bit(unsigned long int *n, unsigned int i);
 
 #endif
