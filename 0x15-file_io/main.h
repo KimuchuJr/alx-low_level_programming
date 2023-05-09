82340bcdf69cc0dacc4c5d21bc8ce613f1a936eb
KimuchuJr <kimuchukim@gmail.com>
2023-05-09 13:00:09 -0700

    Header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,5 +5,8 @@ ssize_t read_textfile(const char *flnm, size_t ltrs);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int _putchar(char c);
+void print_error(char *msg);
+void print_header_info(int fd);
+int main(int argc, char **argv);
 
 #endif
