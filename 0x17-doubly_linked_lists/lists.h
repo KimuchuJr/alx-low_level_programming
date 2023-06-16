ee4e23ff767ce468cd44a648591bb5fc55d9ae51
KimuchuJr <kimuchukim@gmail.com>
2023-06-16 12:13:53 -0700

    Header file

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -21,6 +21,7 @@ typedef struct dlistint_s
 
 size_t print_dlistint(const dlistint_t *head);
 size_t dlistint_len(const dlistint_t *h);
-dlistint_t *add_dnodeint_end(dlistint_t **head, const int num);
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
 
 #endif
