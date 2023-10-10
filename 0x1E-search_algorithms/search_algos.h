c4d1a0c3f3d35f54930b036c4184294796794b32
KimuchuJr <kimuchukim@gmail.com>
2023-10-10 13:42:41 -0700

    Header file

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,40 @@
+#ifndef _SEARCH_ALGOS_
+#define _SEARCH_ALGOS_
+
+/**
+ * struct listint_s - singly linked list
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct listint_s
+{
+	int n;
+	size_t index;
+	struct listint_s *next;
+} listint_t;
+
+/**
+ * struct skiplist_s - Singly linked list with an express lane
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ * @express: Pointer to the next node in the express lane
+ *
+ * Description: singly linked list node structure with an express lane
+ */
+typedef struct skiplist_s
+{
+	int n;
+	size_t index;
+	struct skiplist_s *next;
+	struct skiplist_s *express;
+} skiplist_t;
+
+int linear_search(int *array, size_t size, int value);
+
+#endif
